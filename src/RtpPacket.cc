0ee20fca1765fec6f9cc40ea27bc64a814a6dac2
Philip Herron <philip.herron@googlemail.com>
2017-02-26 05:05:32 +0000

    Use buffer instead of copying

--- src/RtpPacket.cc
+++ src/RtpPacket.cc
@@ -28,10 +28,9 @@
 
 #include <string>
 #include <sstream>
-  using std::ostringstream;
 #include <stdexcept>
-  using std::runtime_error;
 
+// ntohs + htohl
 #include <arpa/inet.h>
 
 struct _RTPHeader {
@@ -69,24 +68,22 @@ Overflow::RtpPacket::RtpPacket(const unsigned char *buffer, uint16_t length): m_
                                                                               m_extensionLength(0),
                                                                               m_payload(nullptr),
                                                                               m_payloadLength(0) {
-    struct _RTPHeader header;
-    memcpy(&header, buffer, sizeof(struct _RTPHeader));
-
-    m_version = static_cast<int>(header.version);
-    m_sequenceNumber = ntohs(header.sequencenumber);
-    m_type = static_cast<int>(header.payloadtype);
-    m_marker = header.marker == 0 ? false : true;
-    m_timestmap = ntohl(header.timestamp);
+    struct _RTPHeader *header = (struct _RTPHeader*)buffer;
+    m_version = static_cast<int>(header->version);
+    m_sequenceNumber = ntohs(header->sequencenumber);
+    m_type = static_cast<int>(header->payloadtype);
+    m_marker = header->marker == 0 ? false : true;
+    m_timestmap = ntohl(header->timestamp);
 
     if (m_version != 2) {
-        ostringstream message;
+        std::ostringstream message;
         message << "Invalid RTP packet version: " << m_version;
-        throw runtime_error{ message.str() };
+        throw std::runtime_error(message.str());
     }
 
-    uint16_t payload_offset = sizeof(struct _RTPHeader) + (header.csrccount * sizeof(uint32_t));
+    uint16_t payload_offset = sizeof(struct _RTPHeader) + (header->csrccount * sizeof(uint32_t));
 
-    bool has_extension = (header.extension == 0) ? false : true;
+    bool has_extension = (header->extension == 0) ? false : true;
     if (has_extension) {
         size_t extension_header_offset = payload_offset;
         struct _RTPExtensionHeader extension_header;
@@ -110,12 +107,5 @@ Overflow::RtpPacket::RtpPacket(const unsigned char *buffer, uint16_t length): m_
     m_payloadLength = length - payload_offset;
     
     m_payload = (unsigned char *)malloc(m_payloadLength);
-    if (!m_payload) {
-        LOG(ERROR) << "SEQ NUM: " << m_sequenceNumber <<
-            " - VERSION: " << m_version <<
-            " - PAYLOAD_LENGTH: " << m_payloadLength <<
-            " - OFFSET: " << payload_offset <<
-            " - TOTAL: " << length;
-    }
     memcpy(m_payload, buffer + payload_offset, m_payloadLength);
 }
